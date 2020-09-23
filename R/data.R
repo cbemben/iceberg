e7090481c5e61c94a2d586f7c19277bb14999042
cbemben <cbemben04@gmail.com>
2020-09-22 21:56:59 -0400

    adding dirs and data

---
+++ R/data.R
@@ -0,0 +1,49 @@
+#' Titanic Passenger Training Dataset
+#'
+#' A dataset containing information for a subset of the
+#' passengers on the titanic. Used for training a predictive
+#' model.
+#'
+#' @format A data frame with 891 rows and 12 variables:
+#' \describe{
+#'   \item{PassengerId}{}
+#'   \item{Survived}{}
+#'   \item{Pclass}{}
+#'   \item{Name}{}
+#'   \item{Sex}{}
+#'   \item{Age}{}
+#'   \item{SibSp}{}
+#'   \item{Parch}{}
+#'   \item{Ticket}{}
+#'   \item{Fare}{}
+#'   \item{Cabin}{}
+#'   \item{Embarked}
+#'   ...
+#' }
+#' @source \url{https://www.kaggle.com/c/titanic/data}
+"titanic-train"
+
+#' Titanic Passenger Testing Dataset
+#'
+#' A dataset containing information for a subset of the
+#' passengers on the titanic. Used for testing the accuracy
+#' of the predictive model.
+#'
+#' @format A data frame with 418 rows and 11 variables:
+#' \describe{
+#'   \item{PassengerId}{}
+#'   \item{Pclass}{}
+#'   \item{Name}{}
+#'   \item{Sex}{}
+#'   \item{Age}{}
+#'   \item{SibSp}{}
+#'   \item{Parch}{}
+#'   \item{Ticket}{}
+#'   \item{Fare}{}
+#'   \item{Cabin}{}
+#'   \item{Embarked}
+#'   ...
+#' }
+#' @source \url{https://www.kaggle.com/c/titanic/data}
+"titanic-test"
+
