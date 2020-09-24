3d6458332c65df9769a3712f45f6ceef8dd91316
cbemben <cbemben04@gmail.com>
2020-09-23 20:34:43 -0400

    more config and metadata changes

---
+++ R/titanic-package.R
@@ -0,0 +1,16 @@
+#' The 'titanic' package.
+#'
+#' @description A DESCRIPTION OF THE PACKAGE
+#'
+#' @docType package
+#' @name titanic-package
+#' @aliases titanic
+#' @useDynLib titanic, .registration = TRUE
+#' @import methods
+#' @import Rcpp
+#' @importFrom rstan sampling
+#'
+#' @references
+#' Stan Development Team (2020). RStan: the R interface to Stan. R package version 2.21.2. https://mc-stan.org
+#'
+NULL
